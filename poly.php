2c3ba2713da6f38d34ddc8db66869e08070742c8
Sanket <sankettandalekar@gmail.com>
2018-06-26 18:28:00 +0530

    Polymorphism 2

--- poly.php
+++ poly.php
@@ -1,5 +1,32 @@
 <?php
 
-echo "hello world";
+interface Shape{
+	public function calculate();
+}
+
+class Circle implements Shape{
+	
+	public function calculate()
+	{
+		return 'Circle : 200';
+	}
+}
+
+class Triangle implements Shape{
+	
+	public function calculate()
+	{
+		return 'Triangle : 200';
+	}
+}
+
+function test(Shape $shape)
+{
+	echo $shape->calculate();
+}
+
+test(new Circle());
+echo '<br/>';
+test(new Triangle());
 
 ?>
